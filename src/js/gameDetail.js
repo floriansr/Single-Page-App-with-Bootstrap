f163a32abaee48d61fe5734565a3fb6e369df8eb
floriansr <sueurfloriansf@gmail.com>
2020-04-30 17:01:52 +0200

    youtube_videos added

--- src/js/gameDetail.js
+++ src/js/gameDetail.js
@@ -8,10 +8,10 @@ const GameDetail = (id) => {
 
       fetch(`${finalURL}`)
         .then((response) => response.json())
-        .then((response) => {
-              console.log(response);
-              return response;
-              })
+        // .then((response) => {
+        //       console.log(response);
+        //       return response;
+        //       })
         .then((response) => {
           let { name, released, description, clip, background_image, slug } = response;
 
@@ -24,6 +24,7 @@ const GameDetail = (id) => {
           articleDOM.querySelector("p.description").innerHTML = description;
 
           fetchScreen(slug);
+          fetchYoutube(slug);
         });
       };
 
@@ -46,7 +47,40 @@ const GameDetail = (id) => {
                         <img src="${screen.image}" class="screenshots">
                         `;
                   });
-              });
+        });
+    }
+
+    const fetchYoutube = (slug) => {
+      let urlYoutube = `https://api.rawg.io/api/games/${slug}/youtube?page=1&page_size=4`
+      
+      fetch(`${urlYoutube}`)
+        .then((response) => response.json())
+        .then((response) => {
+              console.log(response.results);
+              return response;
+              })
+        .then((response) => {
+         
+          let videos = response.results;
+          let videoPrincipale = videos.shift()
+
+          let youtube_videos = document.getElementById("youtube_videos");
+          let youtube_video = document.getElementById("youtube_video");
+
+          youtube_video.src = "https://www.youtube.com/embed/"+ videoPrincipale.external_id
+
+            videos.forEach((video) => {
+                            youtube_videos.innerHTML += `
+
+                            <div>
+                                <iframe width="640" height="360" class="stick youtube w-100" src="https://www.youtube.com/embed/${video.external_id}"></iframe>
+                                <p class="title_font white">${video.name}</p>
+                                <p class="title_font red">${video.channel_title}</p>
+                            </div>
+
+                            `;
+            });
+        })
     }
 
     fetchGame("https://api.rawg.io/api/games/", cleanedArgument);
@@ -71,6 +105,9 @@ const GameDetail = (id) => {
 
 
         <div id="screenshots" class= "space-between"></div>
+        <iframe width="640" height="360" class="stick youtube w-50" id="youtube_video" src=""></iframe>
+        <div id="youtube_videos" class="space-between"></div>
+
       </section>
     `;
 
